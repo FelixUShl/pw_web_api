53fc487ba73e6e648e345dd0b90126eed6e5382f
shlyakhtov <felix.u.shl@gmail.com>
2024-05-27 10:47:42 +0300

    created env

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #"PasswordHash": "88020F057FE7287D8D57494382356F97",
 
-import requests, hashlib
+import requests, hashlib, dotenv, os
 
-login = "admin"
-passwd = "admin"
-serv_addr = "localhost"
+dotenv.load_dotenv(".env")
+login = os.getenv("LOGIN")
+passwd = os.getenv("PASSWD")
+serv_addr = os.getenv("SERV_ADR")
 password_hash = hashlib.md5(f'{passwd}'.encode()).hexdigest().upper()
 password_hash += "F593B01C562548C6B7A31B30884BDE53"
 password_hash = hashlib.md5(password_hash.encode()).hexdigest().upper()
